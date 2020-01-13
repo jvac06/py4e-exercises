8d97a2562a5c77c5a023be77f453428390755772
jesidacosta <jvac06@gmail.com>
2020-01-13 18:32:09 -0500

    easter egg exercise

---
+++ ex_07_03.py
@@ -0,0 +1,19 @@
+#Write an Easter Egg code if someone writes na na boo boo as file name
+fname = input('Enter file name: ')
+if fname == 'na na boo boo':
+    print("NA NA BOO BOO TO YOU - You have been punk'd!")
+
+try:
+    fhandle = open(fname)
+except:
+    print('File name is not recognized', fname)
+    exit()
+
+count = 0
+for line in fhandle:
+    line = line.rstrip()
+    line = line.casefold()
+    if line.startswith('subject:'):
+        count = count + 1
+
+print("There were %d subject lines in %s" (%count, %fname))
