cdb4d6ee58d8553b43a8a141a52d6c86dd1b36e1
Jesid Acosta <Jesid.Acosta2@gerdau.com>
2020-01-15 12:36:21 -0500

    changes for submission

--- ex_06_05.py
+++ ex_06_05.py
@@ -1,6 +1,6 @@
-str = 'X-DSPAM-Confidence:0.8475'
-str_start = str.find(':')
-slice = str[str_start+1:-1]
+text = "X-DSPAM-Confidence:    0.8475"
+str_start = text.find(':')
+slice = text[str_start+4:]
 # print(str_start)
 flt = float(slice)
 print(flt)
